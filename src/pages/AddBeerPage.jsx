b71232f7512438ed9aff13f5b0ba4b8f1ddbe75f
David Masegosa <davidmasegosa@gmail.com>
2026-09-03 16:23:56 +0200

    Create a new beer

--- src/pages/AddBeerPage.jsx
+++ src/pages/AddBeerPage.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API_URL = "https://beers-api.edu.ironhack.com/beers/new";
 
 function AddBeerPage() {
+  const navigate = useNavigate();
+
+
   // State variables to store the values of the form inputs. You can leave these as they are.
   const [name, setName] = useState("");
   const [tagline, setTagline] = useState("");
@@ -23,10 +30,30 @@ function AddBeerPage() {
 
 
 
-  // TASK:
-  // 1. Create a function to handle the form submission and send the form data to the Beers API to create a new beer.
-  // 2. Use axios to make a POST request to the Beers API.
-  // 3. Once the beer is created, navigate the user to the page showing the list of all beers.
+  // Handle the form submission and send the form data to the Beers API to create a new beer.
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newBeer = {
+      name,
+      tagline,
+      description,
+      image_url: imageUrl,
+      first_brewed: firstBrewed,
+      brewers_tips: brewersTips,
+      attenuation_level: Number(attenuationLevel),
+      contributed_by: contributedBy,
+    };
+
+    axios
+      .post(API_URL, newBeer)
+      .then(() => {
+        navigate("/beers");
+      })
+      .catch((error) => {
+        console.log("Error creating the new beer", error);
+      });
+  };
 
 
 
@@ -34,7 +61,7 @@ function AddBeerPage() {
   return (
     <>
       <div className="d-inline-flex flex-column w-100 p-4">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Name</label>
           <input
             className="form-control mb-4"
